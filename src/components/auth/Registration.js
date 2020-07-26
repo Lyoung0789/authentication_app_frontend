1a4d351cd850c45519cba1956fec3025fb5ef9d8
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-25 21:41:53 -0400

    creating the form and handle on Change for registration

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 
 export default class Registration extends Component {
+
+    state = {
+        email: "", 
+        password: "", 
+        passwordConfirmation: "",
+        registrationErrors: ""
+    }
+
+
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("Form submitted")
+    }
+
+    handleChange = (event) => {
+        
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+
     render() {
         return (
             <div>
-                <h3>Registration goes here</h3>
+                <form onSubmit={event => this.handleSubmit(event)}>
+                    <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={event => this.handleChange(event)}/>
+                    <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={event => this.handleChange(event)}/>
+                    <input type="password" name="passwordConfirmation" placeholder="Password Confirmation" value={this.state.passwordConfirmation} onChange={event => this.handleChange(event)}/>
+                    <input type ="submit"/>
+                    
+                </form>
             </div>
         )
     }
