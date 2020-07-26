956f71f91574caa243acc6a966be382ccdbf61e2
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-25 21:26:29 -0400

    createing registration component

---
+++ src/components/auth/Registration.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Registration extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Registration goes here</h3>
+            </div>
+        )
+    }
+}
