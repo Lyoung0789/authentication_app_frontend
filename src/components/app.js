27c4aa5baaaf3ddd99ff8b0ef84e759e46a39dda
Lyoung0789 <Lestermyoung07@gmail.com>
2020-07-27 18:41:32 -0400

    Built checkLoginStatus, correlates the front end and back end of the user that is logged in

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 import Home from "./Home"
 import Dashboard from "./Dashboard"
+import axios from 'axios'
+import { data } from 'autoprefixer';
 
 export default class App extends Component {
 
@@ -16,6 +18,33 @@ export default class App extends Component {
       user: data.user
     })
 
+  }
+
+  checkLoginStatus = () => {
+    console.log("in checkLoginStatus")
+    axios.get("http://localhost:3001/logged_in", {withCredentials: true})
+    .then(response => {
+      if(response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          
+        })
+
+      }
+      console.log("logged in?", response)
+    })
+    .catch(error => {
+      console.log("check login error", error)
+    })
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus()
   }
 
   render() {
